18a6ba8c0cc46ab5cd924b951828afa238db5ab3
Jenkins HMRC <jenkins-hmrc@digital.cabinet-office.gov.uk>
2017-03-14 14:24:00 +0000

    Creating new service cbcr-frontend

---
+++ project/FrontendBuild.scala
@@ -0,0 +1,34 @@
+import sbt._
+import play.sbt.PlayImport._
+import play.core.PlayVersion
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object FrontendBuild extends Build with MicroService {
+
+  val appName = "cbcr-frontend"
+
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.16.0",
+    "uk.gov.hmrc" %% "play-partials" % "5.3.0",
+    "uk.gov.hmrc" %% "play-authorised-frontend" % "6.3.0",
+    "uk.gov.hmrc" %% "play-config" % "4.3.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.2.0",
+    "uk.gov.hmrc" %% "play-health" % "2.1.0",
+    "uk.gov.hmrc" %% "play-ui" % "7.0.1"
+  )
+
+  def test(scope: String = "test") = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % scope,
+    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.pegdown" % "pegdown" % "1.6.0" % scope,
+    "org.jsoup" % "jsoup" % "1.8.1" % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  )
+
+}
