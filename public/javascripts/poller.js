74c282b8b812bdca4435670e4d017211a3a92782
Atul <atuldhk@Atul-Dhokes-MacBook-Pro.local>
2017-03-16 14:45:59 +0000

    Updated with new port numbers

--- public/javascripts/poller.js
+++ public/javascripts/poller.js
@@ -2,14 +2,14 @@
 
 function poll(envelopeId) {
    setTimeout(function() {
-      $.ajax({ url: "http://localhost:9000/cbcr-frontend/getFileuploadResponse/"+envelopeId, success: function(data){
+      $.ajax({ url: "http://localhost:9696/cbcr-frontend/getFileuploadResponse/"+envelopeId, success: function(data){
         var key = "status";
         var value = data[key];
         var eKey = "envelopeId"
         var eVal = data[eKey]
 
         if(eVal == envelopeId && value == 'AVAILABLE'){
-            window.location.href="http://localhost:9000/cbcr-frontend/successFileUpload";
+            window.location.href="http://localhost:9696/cbcr-frontend/successFileUpload";
         } else{
         //Setup the next poll recursively
         poll(envelopeId);
