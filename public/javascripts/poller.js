7735fa830297e64f2106527d25b7cd6c11b71f71
Atul <atuldhk@Atul-Dhokes-MacBook-Pro.local>
2017-03-27 11:31:08 +0100

    Fixed URLS

--- public/javascripts/poller.js
+++ public/javascripts/poller.js
@@ -2,14 +2,14 @@
 
 function poll(envelopeId) {
    setTimeout(function() {
-      $.ajax({ url: "http://www-dev.***REMOVED***/country-by-country-reporting/getFileuploadResponse/"+envelopeId, success: function(data){
+      $.ajax({ url: "https://www-dev.***REMOVED***/country-by-country-reporting/getFileuploadResponse/"+envelopeId, success: function(data){
         var key = "status";
         var value = data[key];
         var eKey = "envelopeId"
         var eVal = data[eKey]
 
         if(eVal == envelopeId && value == 'AVAILABLE'){
-            window.location.href="http://www-dev.***REMOVED***/country-by-country-reporting/successFileUpload";
+            window.location.href="https://www-dev.***REMOVED***/country-by-country-reporting/successFileUpload";
         } else{
         //Setup the next poll recursively
         poll(envelopeId);
