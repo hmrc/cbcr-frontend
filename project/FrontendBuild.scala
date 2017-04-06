84818110e4bf348247f6cc4e50a7b7d7445ab436
Max Worgan <max.worgan@digital.hmrc.gov.uk>
2017-04-06 21:11:27 +0100

    Added SubscriptionSpec

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -32,7 +32,8 @@ object FrontendBuild extends Build with MicroService {
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.8.1" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % scope
+    "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % scope,
+    "org.mockito" % "mockito-core" % "1.9.0" % scope
   )
 
 }
