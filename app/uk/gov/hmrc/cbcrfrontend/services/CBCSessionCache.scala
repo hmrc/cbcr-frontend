be9e7c59c25c28c5af20a8482b0bc252fe55c47f
Max Worgan <max.worgan@gmail.com>
2017-05-08 16:46:11 +0100

    Fixed packageStrip method

--- app/uk/gov/hmrc/cbcrfrontend/services/CBCSessionCache.scala
+++ app/uk/gov/hmrc/cbcrfrontend/services/CBCSessionCache.scala
@@ -43,9 +43,11 @@ class CBCSessionCache @Inject() (val config:Configuration, val http:HttpGet with
   override def domain: String = conf.get[String]("domain").value
 
   def save[T:Writes:TypeTag](body:T)(implicit hc:HeaderCarrier): Future[CacheMap] =
-    cache(typeOf[T].toString.split('.').last,body)
+    cache(stripPackage(typeOf[T].toString),body)
 
   def read[T:Reads:TypeTag](implicit hc:HeaderCarrier): Future[Option[T]] =
-    fetchAndGetEntry(typeOf[T].toString.split(',').last)
+    fetchAndGetEntry(stripPackage(typeOf[T].toString))
 
+
+  def stripPackage(s:String) : String = s.split('.').last
 }
