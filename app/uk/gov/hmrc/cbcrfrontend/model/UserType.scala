e78e73f638d9054d0ac608b55f7eb93a01b19b00
Max Worgan <maxworgan@neovahealth.co.uk>
2017-05-15 11:27:05 +0100

    wip

--- app/uk/gov/hmrc/cbcrfrontend/model/UserType.scala
+++ app/uk/gov/hmrc/cbcrfrontend/model/UserType.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.cbcrfrontend.model
 
 import play.api.libs.json.Json
@@ -6,8 +22,8 @@ import play.api.libs.json.Json
   * Created by max on 15/05/17.
   */
 
-sealed trait UserType
-case object Company extends UserType
+sealed trait UserType extends Product with Serializable
+case object Organisation extends UserType
 case object Agent extends UserType
 
 case class AffinityGroup(affinityGroup: String)
