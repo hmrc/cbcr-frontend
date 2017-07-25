e01ac780769228d0ebb05b8c806941b623e06e13
Max Worgan <max.worgan@digital.hmrc.gov.uk>
2017-07-25 10:48:54 +0100

    Display a page when Agents try to register rather than a 401

--- app/uk/gov/hmrc/cbcrfrontend/auth/CBCRAuthenticationProvider.scala
+++ app/uk/gov/hmrc/cbcrfrontend/auth/CBCRAuthenticationProvider.scala
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2017 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package uk.gov.hmrc.cbcrfrontend.auth
 
 import javax.inject.{Inject, Singleton}
@@ -46,8 +30,11 @@ import uk.gov.hmrc.cbcrfrontend.services.CBCSessionCache
 import uk.gov.hmrc.play.frontend.auth._
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.http.HeaderCarrier
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
 
 import scala.concurrent.{ExecutionContext, Future}
+import uk.gov.hmrc.cbcrfrontend.views.html._
 
 
 trait SecuredActions extends Actions {
@@ -99,9 +86,11 @@ object CBCRPageVisibilityPredicate extends PageVisibilityPredicate {
 class AffinityGroupPredicate(restrictToUser: UserType)(implicit auth: AuthConnector, cache:CBCSessionCache, ec:ExecutionContext)
   extends PageVisibilityPredicate {
 
-  private val errorPage = Future.successful(Unauthorized)
+  private def errorPage(implicit request:Request[_]) = Future.successful(Unauthorized(views.html.subscription.notAuthorised(includes.asideBusiness(), includes.phaseBannerBeta())))
 
   override def apply(authContext: AuthContext, request: Request[AnyContent]): Future[PageVisibilityResult] = {
+
+    implicit val r = request
 
     implicit def hc(implicit request: Request[_]): HeaderCarrier = HeaderCarrier.fromHeadersAndSession(request.headers, Some(request.session))
 
