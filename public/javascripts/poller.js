449832f86949396e930693da083bd94d3bd07e10
Peter <peter@prize.ltd.uk>
2017-08-10 20:24:24 +0100

    CBCR-457: Fixed non dynamic polling and fileupload urls (#109)

--- public/javascripts/poller.js
+++ public/javascripts/poller.js
@@ -1,25 +1,23 @@
+function poll(pollPath, readyPath, virusFailPath, errorPath) {
+    setTimeout(function() {
+        $.ajax({ url: pollPath,
+            success: function(data, statusText, xhr){
+                if(xhr.status == 202) {
+                    window.location.href=readyPath
+                }else{
+                    //Setup the next poll recursively
+                    poll(pollPath, readyPath, virusFailPath, errorPath);
+                }
+            },
+            error: function(xhr,status,error){
+                var errMsg = xhr.responseText;
+                if(xhr.status == 409) {
+                    window.location.href=virusFailPath;
+                } else {
+                    window.location.href=errorPath;
+                }
+            }
 
-
-function poll(envelopeId, fileId, protocolHostName) {
-   setTimeout(function() {
-      $.ajax({ url: protocolHostName+"/country-by-country-reporting/file-upload-response/"+envelopeId+"/"+fileId,
-      success: function(data, statusText, xhr){
-          if(xhr.status == 202) {
-            window.location.href=protocolHostName+"/country-by-country-reporting/file-upload-ready/"+envelopeId + "/" + fileId
-        }else{
-          //Setup the next poll recursively
-          poll(envelopeId, fileId, protocolHostName);
-        }
-      },
-      error: function(xhr,status,error){
-        var errMsg = xhr.responseText;
-          if(xhr.status == 409) {
-            window.location.href=protocolHostName+"/country-by-country-reporting/virus-check-failed";
-          } else {
-            window.location.href= protocolHostName+"/country-by-country-reporting/technical-difficulties";
-          }
-      }
-
-      });
-  }, 3000);
+        });
+    }, 3000);
 }
