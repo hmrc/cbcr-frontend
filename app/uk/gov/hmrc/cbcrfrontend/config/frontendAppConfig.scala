53c8e68b9c3d360a42d7609d34659496cacb81ec
Brian Heathcote <bri.heathcote@googlemail.com>
2017-10-02 13:44:15 +0100

    rolled back frontendAppConfig as schema Version not retrieved via this

--- app/uk/gov/hmrc/cbcrfrontend/config/frontendAppConfig.scala
+++ app/uk/gov/hmrc/cbcrfrontend/config/frontendAppConfig.scala
@@ -32,7 +32,6 @@ trait AppConfig {
   val fileUploadFrontendHost: String
   val betaFeedbackUrlNoAuth: String
   val governmentGatewaySignOutUrl: String
-  val schemaVersion: String
 
 
 }
@@ -69,8 +68,5 @@ object FrontendAppConfig extends AppConfig with ServicesConfig {
 
   lazy val whitelist = whitelistConfig("whitelist")
   lazy val whitelistExcluded = whitelistConfig("whitelist-excluded")
-
-  lazy val schemaVersion = loadConfig(s"oecd-schema-version")
-
 
 }
