a21b3553dc8024c0ea0b6a68df47bcc008279d34
Brian Heathcote <bri.heathcote@googlemail.com>
2017-11-14 11:32:56 +0000

    and again

--- app/uk/gov/hmrc/cbcrfrontend/GuiceModule.scala
+++ app/uk/gov/hmrc/cbcrfrontend/GuiceModule.scala
@@ -20,24 +20,26 @@ import java.io.File
 
 import com.google.inject.AbstractModule
 import org.codehaus.stax2.validation.{XMLValidationSchema, XMLValidationSchemaFactory}
-import play.api.{Configuration, Environment}
+import play.api.{Configuration, Environment, Logger}
 import uk.gov.hmrc.cbcrfrontend.auth.{SecuredActions, SecuredActionsImpl}
 import uk.gov.hmrc.cbcrfrontend.connectors.BPRKnownFactsConnector
 import uk.gov.hmrc.play.config.ServicesConfig
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.http.{HttpGet, HttpPost}
 import uk.gov.hmrc.cbcrfrontend.services.RunMode
+import play.api.Mode._
 
 class GuiceModule(environment: Environment,
                   configuration: Configuration) extends AbstractModule with ServicesConfig {
 
-  val runMode = new RunMode(configuration)
-//  val nowt: String = runMode.env match {
-//    case "Prod" => configuration.getString(s"Prod.oecd-schema-version").getOrElse(throw new Exception(s"Missing configuration Prod.oecd-schema-version"))
-//    case _      => configuration.getString(s"Dev.oecd-schema-version").getOrElse(throw new Exception(s"Missing configuration Dev.oecd-schema-version"))
-//    }
+//  Logger.info(s"environment.mode: ${environment.mode}")
+//  val runMode = new RunMode(configuration)
+  val schemaVer: String = environment.mode match {
+    case Prod => configuration.getString(s"Prod.oecd-schema-version").getOrElse(throw new Exception(s"Missing configuration Prod.oecd-schema-version"))
+    case _      => configuration.getString(s"Dev.oecd-schema-version").getOrElse(throw new Exception(s"Missing configuration Dev.oecd-schema-version"))
+    }
 
-  val schemaVer: String = configuration.getString(s"Dev.oecd-schema-version").getOrElse(throw new Exception(s"Missing configuration Dev.oecd-schema-version"))
+//  val schemaVer: String = configuration.getString(s"Dev.oecd-schema-version").getOrElse(throw new Exception(s"Missing configuration Dev.oecd-schema-version"))
 
 
   override def configure(): Unit = {
