7869de8401372f7792a2e3ec6791fbf51c49f964
Brian Heathcote <bri.heathcote@googlemail.com>
2018-03-20 15:36:14 +0000

    updated dependency versions to allow build

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -11,14 +11,14 @@ object FrontendBuild extends Build with MicroService {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "frontend-bootstrap" % "7.15.0",
-    "uk.gov.hmrc" %% "play-partials" % "5.2.0",
-    "uk.gov.hmrc" %% "play-authorised-frontend" % "6.2.0",
+    "uk.gov.hmrc" %% "play-partials" % "5.4.0",
+    "uk.gov.hmrc" %% "play-authorised-frontend" % "6.4.0",
     "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
-    "uk.gov.hmrc" %% "play-config" % "3.1.0",
+    "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.0.0",
-    "uk.gov.hmrc" %% "play-health" % "2.0.0",
-    "uk.gov.hmrc" %% "play-ui" % "5.4.0",
+    "uk.gov.hmrc" %% "play-health" % "2.1.0",
+    "uk.gov.hmrc" %% "play-ui" % "7.0.0",
     "uk.gov.hmrc" %% "emailaddress" % "2.1.0",
     "org.typelevel" %% "cats" % "0.9.0",
     "org.typelevel" %% "cats-core" % "0.9.0",
@@ -34,7 +34,7 @@ object FrontendBuild extends Build with MicroService {
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "2.2.0" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "2.4.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.8.1" % scope,
