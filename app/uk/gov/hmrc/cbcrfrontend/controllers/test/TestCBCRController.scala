ee00ec7dca2731ac3be284a2505ca57b2b3ce38b
Alok <alok.acharya@digital.hmrc.gov.uk>
2019-04-09 14:16:23 +0100

    DLS-199: Fixed compilation issue

--- app/uk/gov/hmrc/cbcrfrontend/controllers/test/TestCBCRController.scala
+++ app/uk/gov/hmrc/cbcrfrontend/controllers/test/TestCBCRController.scala
@@ -180,18 +180,21 @@ class TestCBCRController @Inject()(val authConnector:AuthConnector,
     }
   }
 
-  def updateReportingEntityAdditionalInfoDRI(docRefId:String) = Action.async{implicit request =>
+  def updateReportingEntityAdditionalInfoDRI(docRefId:String) = Action.async { implicit request =>
     authorised() {
-      testCBCRConnector.updateReportingEntityAdditionalInfoDRI(docRefId).map{s =>
+      testCBCRConnector.updateReportingEntityAdditionalInfoDRI(docRefId).map { s =>
         s.status match {
-          case OK           => Ok("Reporting entity additionalInfoDRI updated")
+          case OK => Ok("Reporting entity additionalInfoDRI updated")
           case NOT_MODIFIED => Ok("Reporting entity additionalInfoDRI NOT updated")
-          case _            => Ok("Something went wrong")
+          case _ => Ok("Something went wrong")
         }
-      }.recover{
-        case _:NotFoundException => Ok("Reporting entity not found")
+      }.recover {
+        case _: NotFoundException => Ok("Reporting entity not found")
+      }
+    }
+  }
 
-  def retrieveSchemaValidationErrors() = Action.async{ implicit request =>
+  def retrieveSchemaValidationErrors() = Action.async { implicit request =>
     authorised() {
       OptionT(cache.readOption[XMLErrors]).map(x => fileUploadService.errorsToString(List(x))).fold (
         NoContent
