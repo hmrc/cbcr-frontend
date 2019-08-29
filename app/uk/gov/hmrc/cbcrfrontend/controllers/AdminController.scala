de1ad3311b3de64b5ba5da1a7bb16d29f537351e
Alok <alok.acharya@digital.hmrc.gov.uk>
2019-08-29 13:14:30 +0100

    DLS-919: Added new endpoint

--- app/uk/gov/hmrc/cbcrfrontend/controllers/AdminController.scala
+++ app/uk/gov/hmrc/cbcrfrontend/controllers/AdminController.scala
@@ -20,29 +20,89 @@ import java.time.LocalDate
 
 import javax.inject.Inject
 import play.api.Configuration
-import play.api.data.Form
-import play.api.data.Forms.{localDate, mapping, nonEmptyText}
+import play.api.data.{Form, FormError}
+import play.api.data.Forms.{list, localDate, mapping, nonEmptyText, of, optional}
+import play.api.data.format.{Formats, Formatter}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json._
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.cbcrfrontend.config.FrontendAppConfig
 import uk.gov.hmrc.cbcrfrontend.connectors.CBCRBackendConnector
-import uk.gov.hmrc.cbcrfrontend.model.ReportingEntityData
+import uk.gov.hmrc.cbcrfrontend.model.{DocRefId, ReportingEntityData}
 import uk.gov.hmrc.cbcrfrontend.services.ReportingEntityDataService
 import uk.gov.hmrc.cbcrfrontend.views.html._
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 
+
 import scala.concurrent.{ExecutionContext, Future}
 
+/*This case class is used to show all the DocRefIds stored in our mongo DB. The one we had in models had too many parameters and we only retrieve id and valid fields from DB which isnt enough to make the original
+* case class.
+* */
+case class AdminDocRefId(id: String)
+
+object AdminDocRefId {
+
+  implicit val format: Format[AdminDocRefId] = new Format[AdminDocRefId] {
+    override def writes(o: AdminDocRefId): JsValue = JsString(o.id)
+
+    override def reads(json: JsValue): JsResult[AdminDocRefId] = json.asOpt[JsString].map(v => AdminDocRefId(v.value)).fold[JsResult[AdminDocRefId]](
+      JsError(s"Unable to deserialise $json as a DocRefId"))(
+      (id: AdminDocRefId) => JsSuccess(id)
+    )
+  }
+
+  implicit val adminDocRefIdFormatter = new Formatter[AdminDocRefId] {
+    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], AdminDocRefId] =
+      Formats.stringFormat.bind(key, data).right.map(string => AdminDocRefId(string))
+
+    override def unbind(key: String, value: AdminDocRefId): Map[String, String] = Map(key -> value.id)
+  }
+}
+
+/*This case class is used to re validate Doc Ref Ids that has been invalidated by users.*/
+case class AdminDocRefIdRecord(id: AdminDocRefId, valid: Boolean)
+
+object AdminDocRefIdRecord {
+
+  implicit val format: Format[AdminDocRefIdRecord] = Json.format[AdminDocRefIdRecord]
+}
+
+case class ListDocRefIdRecord(docs: List[AdminDocRefIdRecord])
+
+object ListDocRefIdRecord {
+  implicit val format: Format[ListDocRefIdRecord] = Json.format[ListDocRefIdRecord]
+}
+
+/*This case class is used when searching for reporting entity data using cbc Id date */
+case class AdminCbcIdAndDate(cbcId: String, date: LocalDate)
+
+/*This case class is used when searching for reporting entity data tin cbc Id date */
+case class AdminTinAndDate(tin: String, date: LocalDate)
+
+/*This case class is used when we manually need to the customers reporting entity and docref if and addtional info.*/
+case class AdminReportingEntityDataRequestForm(selector: AdminDocRefId, cbcReportsDRI: List[AdminDocRefId], additionalInfoDRI: Option[List[AdminDocRefId]], reportingEntityDRI: AdminDocRefId)
+
+object AdminReportingEntityDataRequestForm {
+  implicit val format = Json.format[AdminReportingEntityDataRequestForm]
+  }
+
+case class AdminReportingEntityData(cbcReportsDRI: List[AdminDocRefId], additionalInfoDRI: Option[List[AdminDocRefId]], reportingEntityDRI: AdminDocRefId)
+
+object AdminReportingEntityData {
+  implicit val format = Json.format[AdminReportingEntityData]
+}
+
+
 class AdminController @Inject()(frontendAppConfig: FrontendAppConfig,
-                                val config:Configuration,
-                                val audit:AuditConnector,
+                                val config: Configuration,
+                                val audit: AuditConnector,
                                 cbcrBackendConnector: CBCRBackendConnector,
                                 reportingEntityDataService: ReportingEntityDataService)
-                               (implicit conf:FrontendAppConfig,
-                                override val messagesApi:MessagesApi,
+                               (implicit conf: FrontendAppConfig,
+                                override val messagesApi: MessagesApi,
                                 val ec: ExecutionContext,
                                 messagesControllerComponents: MessagesControllerComponents) extends FrontendController(messagesControllerComponents) with I18nSupport {
 
@@ -51,9 +111,14 @@ class AdminController @Inject()(frontendAppConfig: FrontendAppConfig,
 
   lazy val credentials = Creds(frontendAppConfig.username, frontendAppConfig.password)
 
-  case class AdminDocRefId(id:String)
-  case class AdminCbcIdAndDate(cbcId:String, date: LocalDate)
-  case class AdminTinAndDate(tin:String, date: LocalDate)
+  val adminReportingEntityDataRequestForm = Form(
+    mapping(
+      "selector" -> of[AdminDocRefId],
+      "cbcReportsDRI" -> nonEmptyText.transform[List[AdminDocRefId]](_.split(",").toList.map(elem => AdminDocRefId(elem)), l => l.map(_.id).mkString),
+      "additionalInfoDRI" -> optional(nonEmptyText).transform[Option[List[AdminDocRefId]]](str => str.map(in => in.split(",").toList.map(AdminDocRefId(_))), list => list.map(l => l.map(_.id).mkString)),
+      "reportingEntityDRI" -> of[AdminDocRefId]
+    )(AdminReportingEntityDataRequestForm.apply)(AdminReportingEntityDataRequestForm.unapply)
+  )
 
   val adminQueryDocRefIdForm: Form[AdminDocRefId] = Form(
     mapping(
@@ -85,8 +150,8 @@ class AdminController @Inject()(frontendAppConfig: FrontendAppConfig,
     implicit request =>
       cbcrBackendConnector.getDocRefIdOver200.map(
         documents => Ok(show_all_docRefIds(documents.docs))
-  )
-}
+      )
+  }
 
   def showAmendDocRefIdPage = AuthenticationController(credentials).apply {
     implicit request =>
@@ -139,29 +204,29 @@ class AdminController @Inject()(frontendAppConfig: FrontendAppConfig,
       )
   }
 
-}
 
-
-case class AdminDocRefId(id:String)
-object AdminDocRefId {
-  implicit val format: Format[AdminDocRefId] = new Format[AdminDocRefId] {
-    override def writes(o: AdminDocRefId): JsValue = JsString(o.id)
-
-    override def reads(json: JsValue): JsResult[AdminDocRefId] = json.asOpt[JsString].map(v => AdminDocRefId(v.value)).fold[JsResult[AdminDocRefId]](
-      JsError(s"Unable to deserialise $json as a DocRefId"))(
-      (id: AdminDocRefId) => JsSuccess(id)
-    )
+  def showEditReportingEntityPage = AuthenticationController(credentials).async {
+    implicit request =>
+      Future.successful(Ok(adminEditReportingEntityData(adminReportingEntityDataRequestForm)))
   }
-}
 
-case class AdminDocRefIdRecord(id:AdminDocRefId,valid:Boolean)
-object AdminDocRefIdRecord {
 
-implicit val format:Format[AdminDocRefIdRecord] = Json.format[AdminDocRefIdRecord]
-}
+  def editReportingEntity = AuthenticationController(credentials).async {
+    implicit request =>
+      adminReportingEntityDataRequestForm.bindFromRequest().fold(
+        errors => Future.successful(BadRequest("Error")),
+        editForm => {
+          val adminReportingEntityData = AdminReportingEntityData(editForm.cbcReportsDRI, editForm.additionalInfoDRI, editForm.reportingEntityDRI)
+          cbcrBackendConnector.editAdminReportingEntity(editForm.selector, adminReportingEntityData).map(
+            _ => Ok(s"Replaced document with ${editForm.selector} field with ${Json.toJson(adminReportingEntityData)}")
 
-case class ListDocRefIdRecord(docs: List[AdminDocRefIdRecord])
-object ListDocRefIdRecord {
-  implicit val format:Format[ListDocRefIdRecord] = Json.format[ListDocRefIdRecord]
+          )
+        }
+
+      )
+
+
+  }
+
 }
 
