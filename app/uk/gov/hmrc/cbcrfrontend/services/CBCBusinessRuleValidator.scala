23acc4d5f065a7d07f0d6d6e28d7dc485551b3ba
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2020-10-23 11:38:23 +0100

    [CU][HTSD-2]: Update logic to ignore checks against OECD3 doc ref ids for a full currency change

--- app/uk/gov/hmrc/cbcrfrontend/services/CBCBusinessRuleValidator.scala
+++ app/uk/gov/hmrc/cbcrfrontend/services/CBCBusinessRuleValidator.scala
@@ -712,7 +712,8 @@ class CBCBusinessRuleValidator @Inject()(
               case Some(reportEntityData) =>
                 reportEntityData.currencyCode match {
                   case Some(code) =>
-                    val reports: List[String] = reportEntityData.cbcReportsDRI.map(_.show).toList
+                    val reports: List[String] =
+                      reportEntityData.cbcReportsDRI.filterNot(_.docTypeIndic == OECD3).map(_.show)
                     val corrDocRefIds: List[String] = x.cbcReport
                       .map(_.docSpec.corrDocRefId)
                       .filter(_.isDefined)
