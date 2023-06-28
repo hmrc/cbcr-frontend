2edc93f82dadcbde74b9d81b3052755b7a1c8ecf
johno1985 <1640705+johno1985@users.noreply.github.com>
2023-06-28 18:36:04 +0100

    DLS-6253: Fixed dependeny scopes
     - moved runtime dependencies from test scope to compile scope

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,23 +7,23 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.25.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "5.0.0-play-28",
-    "uk.gov.hmrc"       %% "emailaddress"               % "3.7.0",
-    "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
-    "uk.gov.hmrc"       %% "http-caching-client"        % "9.6.0-play-28",
-    "org.typelevel"     %% "cats-core"                  % "1.0.0",
-    "com.github.kxbmap" %% "configs"                    % "0.6.0",
-    "com.scalawilliam"  %% "xs4s"                       % "0.5",
-    "commons-io"        % "commons-io"                  % "2.6",
-    "org.mindrot"       % "jbcrypt"                     % "0.4"
+    "uk.gov.hmrc"              %% "bootstrap-frontend-play-28" % "5.25.0",
+    "uk.gov.hmrc"              %% "play-frontend-hmrc"         % "5.0.0-play-28",
+    "uk.gov.hmrc"              %% "emailaddress"               % "3.7.0",
+    "uk.gov.hmrc"              %% "domain"                     % "8.1.0-play-28",
+    "uk.gov.hmrc"              %% "http-caching-client"        % "9.6.0-play-28",
+    "org.typelevel"            %% "cats-core"                  % "1.0.0",
+    "com.github.kxbmap"        %% "configs"                    % "0.6.0",
+    "com.scalawilliam"         %% "xs4s"                       % "0.5",
+    "commons-io"               % "commons-io"                  % "2.6",
+    "org.mindrot"              % "jbcrypt"                     % "0.4",
+    "com.sun.msv.datatype.xsd" % "xsdlib"                      % "2013.2",
+    "msv"                      % "msv"                         % "20050913",
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"              %% "bootstrap-test-play-28" % hmrcBootstrapVersion % scope,
-    "org.mockito"              %% "mockito-scala"          % mockitoScalaVersion % scope,
-    "org.mockito"              %% "mockito-scala-cats"     % mockitoScalaVersion % scope,
-    "com.sun.msv.datatype.xsd" % "xsdlib"                  % "2013.2" % scope,
-    "msv"                      % "msv"                     % "20050913" % scope,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % hmrcBootstrapVersion % scope,
+    "org.mockito" %% "mockito-scala"          % mockitoScalaVersion  % scope,
+    "org.mockito" %% "mockito-scala-cats"     % mockitoScalaVersion  % scope
   )
 }
