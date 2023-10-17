d085e676b0c4280c5c80ec610baa32aebb5eea11
Tom Rafferty <86563641+TomRafferty@users.noreply.github.com>
2023-10-17 13:37:01 +0100

    DLS-9034 - Updated http-caching-client 9.6.0 => 10.0.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"              %% "play-frontend-hmrc"         % "7.14.0-play-28",
     "uk.gov.hmrc"              %% "emailaddress"               % "3.7.0",
     "uk.gov.hmrc"              %% "domain"                     % "8.1.0-play-28",
-    "uk.gov.hmrc"              %% "http-caching-client"        % "9.6.0-play-28",
+    "uk.gov.hmrc"              %% "http-caching-client"        % "10.0.0-play-28",
     "org.typelevel"            %% "cats-core"                  % "2.0.0",
     "com.github.kxbmap"        %% "configs"                    % "0.6.0",
     "org.codehaus.woodstox"    % "stax2-api"                   % "3.1.4",
