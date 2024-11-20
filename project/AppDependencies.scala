dceaeff0693bf82981fae840e5215b25e9c680f5
karolina-zaborska <112881943+karolina-zaborska@users.noreply.github.com>
2024-11-20 16:40:40 +0000

    DLS-10863: Upgrade app dependencies to the latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
   val hmrcBootstrapVersion = "8.5.0"
-  val mockitoScalaVersion = "1.17.31"
+  val mockitoScalaVersion = "1.17.37"
   val hmrcMongoVersion = "1.4.0"
   val playVersion = "play-30"
 
@@ -13,13 +13,13 @@ object AppDependencies {
     "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion" % hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-$playVersion"         % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "emailaddress"                     % "3.7.0",
-    "uk.gov.hmrc"             %% s"domain-$playVersion"             % "9.0.0",
+    "uk.gov.hmrc"             %% s"domain-$playVersion"             % "10.0.0",
     "org.typelevel"           %% "cats-core"                        % "2.12.0",
-    "org.codehaus.woodstox"    % "stax2-api"                        % "3.1.4",
+    "org.codehaus.woodstox"    % "stax2-api"                        % "4.2.2",
     "org.codehaus.woodstox"    % "woodstox-core-asl"                % "4.4.1",
-    "commons-io"               % "commons-io"                       % "2.6",
+    "commons-io"               % "commons-io"                       % "2.16.1",
     "org.mindrot"              % "jbcrypt"                          % "0.4",
-    "com.sun.msv.datatype.xsd" % "xsdlib"                           % "2013.2",
+    "com.sun.msv.datatype.xsd" % "xsdlib"                           % "20060615",
     "msv"                      % "msv"                              % "20050913"
   )
 
@@ -29,6 +29,6 @@ object AppDependencies {
     "org.mockito"           %% "mockito-scala"                 % mockitoScalaVersion  % scope,
     "org.mockito"           %% "mockito-scala-cats"            % mockitoScalaVersion  % scope,
     "com.github.tomakehurst" % "wiremock"                      % "3.0.0-beta-7"       % scope,
-    "xerces"                 % "xercesImpl"                    % "2.12.0"             % scope
+    "xerces"                 % "xercesImpl"                    % "2.12.2"             % scope
   )
 }
