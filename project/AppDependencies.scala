91144d9160f7879bd643125d6e918c08b728e1cb
ShivangiShah42 <177633426+ShivangiShah42@users.noreply.github.com>
2025-11-10 12:03:45 +0000

    DLS-12401: Enable service navigation component - Gov Uk brand refresh

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import play.sbt.PlayImport.ws
 import sbt.*
 
 object AppDependencies {
-  val hmrcBootstrapVersion = "10.1.0"
+  val hmrcBootstrapVersion = "10.4.0"
   val mockitoScalaVersion = "3.2.18.0"
-  val hmrcMongoVersion = "2.7.0"
+  val hmrcMongoVersion = "2.10.0"
   val playVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"             %% s"bootstrap-frontend-$playVersion" % hmrcBootstrapVersion,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion" % "12.12.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion" % "12.20.0",
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-$playVersion"         % hmrcMongoVersion,
     "uk.gov.hmrc"             %% s"domain-$playVersion"             % "13.0.0",
     "org.typelevel"           %% "cats-core"                        % "2.13.0",
